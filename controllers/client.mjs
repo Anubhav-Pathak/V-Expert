69047f0e2f3275859690e8a40bde1c107c7f3f37
AjPitaji <89189452+AjPitaji@users.noreply.github.com>
2022-11-10 20:17:41 +0530

    Update client.mjs

--- controllers/client.mjs
+++ controllers/client.mjs
@@ -1,11 +1,11 @@
 import express from "express";
 const getHome = (req, res, next) => {
-    // res.render("client/index");
-    res.send("<h1>Hello World</h1>")
+    res.render("client/index");
+//     res.send("<h1>Hello World</h1>")
 }
 export const getCart = (req,res,next)=>{
-    // res.render("client/cart");
-    res.send("<h1>cart section<h1>")
+    res.render("client/cart");
+//     res.send("<h1>cart section<h1>")
 }
 export const getServices = (req,res,next)=>{
     res.render("client/services",{});
