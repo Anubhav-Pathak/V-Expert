e6e92513535952d84413495123d8c32473c14ff0
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-01 22:16:04 +0530

    Added: functionality to Packages

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Header from "./components/layouts/header/header";
 import Home from "./components/pages/home/home";
+import Business from "./components/pages/package/business";
+import Economy from "./components/pages/package/economy";
+import First from "./components/pages/package/first";
+import Package from "./components/pages/package/package";
 function App() {
     return (
         <>
             <Header />
             <Routes>
                 <Route path={"/"} element={<Home />} />
+                <Route path={"/packages"} element={<Package />} />
+                <Route path={"/packages/first"} element={<First />} />
+                <Route path={"/packages/economy"} element={<Economy />} />
+                <Route path={"/packages/business"} element={<Business />} />
             </Routes>
         </>
     );
