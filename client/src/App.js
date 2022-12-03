42e4b62da33361761ae09fc10d6dddc96109e098
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-03 22:54:16 +0530

    Updated: form.js with dynamic content

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,10 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Header from "./components/layouts/header/header";
 import Home from "./components/pages/home/home";
-import Business from "./components/pages/package/business";
-import Economy from "./components/pages/package/economy";
-import First from "./components/pages/package/first";
 import Package from "./components/pages/package/package";
 import Error from "./components/pages/error/error";
+import Form from "./components/pages/package/form";
+
 function App() {
     return (
         <>
@@ -14,9 +13,8 @@ function App() {
             <Routes>
                 <Route path={"/"} element={<Home />} />
                 <Route path={"/packages"} element={<Package />} />
-                <Route path={"/packages/first"} element={<First />} />
-                <Route path={"/packages/economy"} element={<Economy />} />
-                <Route path={"/packages/business"} element={<Business />} />
+                <Route path={"/packages/:name"} element={<Form />} />
+                <Route path={"/error"} element={<Error />} />
                 <Route path={"*"} element={<Error />} />
             </Routes>
         </>
