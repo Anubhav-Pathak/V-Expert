f75f7f0a6f0b4ea718959a7c464015f9c66bee9b
Anubhav Pathak <anubhavpathak99@gmail.com>
2022-12-04 19:45:21 +0530

    Added initial Footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Header from "./components/layouts/header/header";
 import Home from "./components/pages/home/home";
-import Package from "./components/pages/package/package";
+// import Package from "./components/pages/package/package";
+// import Form from "./components/pages/package/form";
 import Error from "./components/pages/error/error";
-import Form from "./components/pages/package/form";
+import Footer from "./components/layouts/footer/footer";
 
 function App() {
     return (
@@ -12,11 +13,12 @@ function App() {
             <Header />
             <Routes>
                 <Route path={"/"} element={<Home />} />
-                <Route path={"/packages"} element={<Package />} />
-                <Route path={"/packages/:name"} element={<Form />} />
+                {/* <Route path={"/packages"} element={<Package />} />
+                <Route path={"/packages/:name"} element={<Form />} /> */}
                 <Route path={"/error"} element={<Error />} />
                 <Route path={"*"} element={<Error />} />
             </Routes>
+            <Footer />
         </>
     );
 }
