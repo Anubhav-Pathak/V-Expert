0fbc1c361357464d97176432f98e6ad498168f37
Pulkit <pulkitmlk1802@gmail.com>
2022-12-04 23:36:53 +0530

    Image Changes

--- client/src/components/pages/home/serviceCarousel.js
+++ client/src/components/pages/home/serviceCarousel.js
@@ -6,10 +6,14 @@ import "swiper/css/pagination";
 import "swiper/css/autoplay"
 import "swiper/css/navigation"
 
-import image1 from "../../../assests/images/img_1.png"
-import image2 from "../../../assests/images/pexels-anete-lusina-4792713.jpg"
-import image3 from "../../../assests/images/pexels-field-engineer.jpg"
-import image4 from "../../../assests/images/pexels-kindel-media-9875405.jpg"
+import electrical from "../../../assests/images/services/electrical.jpg"
+import plumbing from "../../../assests/images/services/plumbing.jpg"
+import plastering from "../../../assests/images/services/plastring.jpg"
+import airCond from "../../../assests/images/services/airCond.jpg"
+import falseCeiling from "../../../assests/images/services/falseCeling.jpg"
+// import image2 from "../../../assests/images/pexels-anete-lusina-4792713.jpg"
+// import image3 from "../../../assests/images/pexels-field-engineer.jpg"
+// import image4 from "../../../assests/images/pexels-kindel-media-9875405.jpg"
 import ServiceSlide from "./serviceSlide";
 
 const ServiceCarousel = (props) => {
@@ -17,42 +21,42 @@ const ServiceCarousel = (props) => {
     const Services = [
         {
             path: "air-conditioning",
-            img: image1,
+            img: airCond,
             icon: "fa-light fa-air-conditioner",
             name: "Air Conditioning",
             description: "The installation, testing& commissioning, maintenance of the air conditioning and ventilation system shall be carried out by a specialist in order to ensure proper operation."
         },
         {
             path: "plumbing",
-            img: image1,
+            img: plumbing,
             icon: "fa-light fa-air-conditioner",
             name: "Plumbing",
             description: "The installation, testing& commissioning, maintenance of the Plumbing system shall be carried out by a specialist in order to ensure proper operation"
         },
         {
             path: "electrical",
-            img: image1,
+            img: electrical,
             icon: "fa-light fa-air-conditioner",
             name: "Electrical",
             description: "The installation, testing& commissioning, maintenance of the Electrical & ELV system shall be carried out by a specialist in order to ensure proper operation."
         },
         {
             path: "plaster",
-            img: image1,
+            img: plastering,
             icon: "fa-light fa-air-conditioner",
             name: "Plaster",
             description: "We have tremendous experience and expertise in the field and are able to deliver outstanding plaster services. We undertake both official and residential requirements."
         },
         {
             path: "floor-tiling",
-            img: image1,
+            img: electrical,
             icon: "fa-light fa-air-conditioner",
             name: "Floor Tiling",
             description: "We specialize in tiles, marble, stone masonry, fill and all kinds of decorating. We cater to all types of domestic, commercial or business tiling services."
         },
         {
             path: "false-ceiling",
-            img: image1,
+            img: falseCeiling,
             icon: "fa-light fa-air-conditioner",
             name: "False Ceiling",
             description: "We specialize in installing false (suspended) ceilings and partitioning covering all commercial and domestic areas across."
