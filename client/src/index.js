579a083613eeb8d7858416bdc9df087b839cdeaf
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-06 22:59:27 +0530

    Updated: index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
