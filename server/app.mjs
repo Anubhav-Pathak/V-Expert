1d708718b0a774cc0d6da1a3f181f1ec6f683a82
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-06 22:59:06 +0530

    Updated: app.mjs

--- server/app.mjs
+++ server/app.mjs
@@ -1,5 +1,25 @@
 import express from "express";
 import path from "path";
-const PORT = process.env.PORT || 3000;
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 8000;
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", function(_, res) {
+    res.sendFile(
+        path.join(__dirname, "../client/build", "index.html"),
+        function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        }
+    )
+})
+
 app.listen(PORT,()=>{console.log(`Server started on port ${PORT}`)});
