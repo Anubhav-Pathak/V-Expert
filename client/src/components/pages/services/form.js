d625c21ef3e122505b98201b70b61af27b9b0de3
Pulkit <pulkitmlk1802@gmail.com>
2022-12-20 21:39:18 +0530

    Single Service Page

---
+++ client/src/components/pages/services/form.js
@@ -0,0 +1,61 @@
+import {useEffect, useState} from "react";
+
+export default ({price}) => {
+
+    const [form, setForm] = useState({
+        size1: String,
+        size2: String,
+        size3: String,
+        price: Number
+    })
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+
+        setForm(prevState => {
+            return {
+                ...prevState,
+                [e.target.name]: e.target.value,
+
+                // Logic for price change
+            //     Else make await this function
+
+            }
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    //     Backend Request
+    }
+
+    useEffect(() => {
+        setForm(prevState => {
+            return {
+                ...prevState,
+                price: price,
+            }
+        })
+    },[]);
+
+    return (
+        <div>
+            <p className={"service-price text-5xl"}>Est Price <span className={"text-2xl"}>{price}</span></p>
+            <form className={"service-form"} onSubmit={handleSubmit}>
+                <label htmlFor={"size1"} className={"service-label"}>
+                    <i className="fa-solid fa-house"></i>
+                    <input id={"size1"} name={"size1"} type={"text"} value={form.size1} placeholder={"Apartment Size"} onChange={handleChange}/>
+                </label>
+                <label htmlFor={"size2"} className={"service-label"}>
+                    <i className="fa-solid fa-house"></i>
+                    <input id={"size2"} name={"size2"} type={"text"} value={form.size2} placeholder={"Apartment Size"} onChange={handleChange}/>
+                </label>
+                <label htmlFor={"size3"} className={"service-label"}>
+                    <i className="fa-solid fa-house"></i>
+                    <input id={"size3"} name={"size2"} type={"text"} value={form.size3} placeholder={"Apartment Size"} onChange={handleChange}/>
+                </label>
+                <button type={"submit"} className={"form-submit-btn text-2xl"}>Make Purchase </button>
+            </form>
+        </div>
+    )
+}
---
+++ client/src/components/pages/services/service.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+import "../../../assests/css/services.css"
+import {services} from "../../../data/data";
+import About from "./about";
+import Form from "./form";
+
+export default () => {
+
+    const {name} = useParams();
+    const selectedService = services.find(s => s.name.toLowerCase() === name);
+
+    return (
+        <section id={"section-service"}>
+            <div className={"single-service-container"}>
+                <About img={selectedService.img} name={selectedService.name} disc={selectedService.description} />
+                <Form price={300}/>
+            </div>
+        </section>
+    )
+}
