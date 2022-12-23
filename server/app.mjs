c348fa9144f6e2ef0e723f91f526673a00d7322d
Anubhav Pathak <anubhavpathak99@gmail.com>
2022-12-23 16:55:47 +0530

    Fixed Server Crash

--- server/app.mjs
+++ server/app.mjs
@@ -1,26 +1,26 @@
-import * as dotenv from 'dotenv';
-dotenv.config()
+// import * as dotenv from 'dotenv';
+// dotenv.config()
 
 import express from "express";
-import path from "path";
-import mongoose from "mongoose";
-import {fileURLToPath} from 'url';
+// import path from "path";
+// import mongoose from "mongoose";
+// import {fileURLToPath} from 'url';
 
-import Package from "./models/package.mjs";
+// import Package from "./models/package.mjs";
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(express.json())
 
-const DATABASE_URL = process.env.DATABASE_URL
-mongoose.connect(DATABASE_URL);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", () => {});
+// const DATABASE_URL = process.env.DATABASE_URL
+// mongoose.connect(DATABASE_URL);
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", () => {});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "../client/build")));
@@ -37,14 +37,14 @@ app.get("*", function(_, res) {
 })
 
 
-app.post("/api/package", async (req, res) => {
-    const data = req.body;
-    const pack = await Package.create(data);
-    try {
-      res.send(pack.id);
-    } catch (error) {
-      res.status(500).send("Error");
-    }
-});
+// app.post("/api/package", async (req, res) => {
+//     const data = req.body;
+//     const pack = await Package.create(data);
+//     try {
+//       res.send(pack.id);
+//     } catch (error) {
+//       res.status(500).send("Error");
+//     }
+// });
 
 app.listen(PORT,()=>{console.log(`Server started on port ${PORT}`)});
