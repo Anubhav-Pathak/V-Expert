247c5df2c9044a6c63744b44ed8d51200152653d
Pulkit <pulkitmlk1802@gmail.com>
2022-12-30 12:09:38 +0530

    Admin Panel

---
+++ admin-client/src/index.js
@@ -0,0 +1,13 @@
+import "./assets/css/globals.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter basename='/'>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
