15382eec565ee965ae14a007c1cf5e43d123b61f
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-30 21:47:08 +0530

    Updated: app.mjs

--- server/app.mjs
+++ server/app.mjs
@@ -1,8 +1,27 @@
 import express from "express";
+import path from "path";
+import {fileURLToPath} from 'url';
+
 import {BookingRouter} from "./routes/bookingRouter.mjs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", function(_, res) {
+    res.sendFile(
+        path.join(__dirname, "../client/build", "index.html"),
+        function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        }
+    )
+})
 
 app.use('/v1/bookings', BookingRouter);
 
