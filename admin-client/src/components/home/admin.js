03fb11dfe8815381b00b274b58c10f5b060222f1
Arvind-4 <lordarvindthegamer@gmail.com>
2022-12-30 21:47:31 +0530

    Removed: unused files

--- admin-client/src/components/home/admin.js
+++
@@ -1,9 +0,0 @@
-import Requests from "./requests";
-
-export default () => {
-    return (
-        <section id={"admin"} className={"min-h-[100vh] py-8 bg-slate-200"}>
-            <Requests />
-        </section>
-    )
-}
