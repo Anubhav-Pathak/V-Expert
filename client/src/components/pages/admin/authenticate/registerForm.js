bc41fc7e2e3a9c2b1e4f91b99991189cc24edecf
Pulkit <pulkitmlk1802@gmail.com>
2022-12-31 16:27:46 +0530

    Admin signin/signup

--- client/src/components/pages/admin/authenticate/registerForm.js
+++ client/src/components/pages/admin/authenticate/registerForm.js
@@ -1,12 +1,16 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {signUp} from "../api";
+import {useNavigate} from "react-router-dom";
 
 export default () => {
 
     const [formData, setFormData] = useState({
-        user: String,
+        email: String,
         name: String,
         password: String
     });
+
+    const navigate = useNavigate();
 
     const changeData = (e) => {
         setFormData(prevState => {
@@ -23,7 +27,18 @@ export default () => {
         e.preventDefault();
         //     Backend Request
         console.log(formData);
+        signUp(formData)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(e => console.log(e));
     }
+
+    useEffect(() => {
+        const user = localStorage.getItem('user');
+        if(!user)
+            navigate("/admin/login");
+    }, []);
 
     return (
         <>
@@ -31,8 +46,8 @@ export default () => {
                 <div className={"text-center text-2xl mb-8 text-cyan-600"}>Create New User</div>
                 <label className={"text-xl"} htmlFor={'name'}>Name</label>
                 <input className={"form-input"} id={"name"} name={"name"} placeholder={"John"} value={formData.name} required={true} onChange={changeData}/>
-                <label className={"text-xl"} htmlFor={'user'}>Username</label>
-                <input className={"form-input"} id={"user"} name={"user"} placeholder={"Username"} value={formData.user} required={true} onChange={changeData}/>
+                <label className={"text-xl"} htmlFor={'email'}>Email</label>
+                <input className={"form-input"} id={"email"} name={"email"} placeholder={"example@example.com"} value={formData.email} required={true} onChange={changeData}/>
                 <label className={"text-xl"} htmlFor={'password'}>Password</label>
                 <input className={"form-input"} id={"password"} name={"password"} placeholder={"Password"} type={"password"} value={formData.password} required={true} onChange={changeData}/>
                 <input className={"form-input cursor-pointer duration-300 mt-4 hover:bg-blue-700 hover:text-white"} type={"submit"} value={"Login"}/>
