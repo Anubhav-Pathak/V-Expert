9047384c593d2c611100d6eeb524c78eec3ec642
Pulkit <pulkitmlk1802@gmail.com>
2023-01-01 08:29:38 +0530

    Admin Account

--- client/src/components/pages/admin/admin-header.js
+++ client/src/components/pages/admin/admin-header.js
@@ -10,7 +10,7 @@ export default () => {
     }
 
     return (
-        <div className={"fixed w-full top-0 left-0 flex justify-end space-x-5 items-center py-2 bg-white shadow-md shadow-black"}>
+        <div className={"fixed w-full top-0 left-0 flex justify-end space-x-5 items-center py-2 bg-white shadow-md shadow-black px-12"}>
             <a href={"/admin/register"} className={"text-lg pb-1 border-b border-b-white duration-300 hover:border-b-black"}>Create</a>
             <button className={"text-lg pb-1 border-b border-b-white duration-300 cursor-pointer hover:border-b-black"} onClick={handleLogout}>Log Out</button>
         </div>
