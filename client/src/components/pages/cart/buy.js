b6ccbb1cefd21a9b8ed36482f18a8bc95c91f3ee
Arvind-4 <lordarvindthegamer@gmail.com>
2023-01-10 21:03:20 +0530

    Updated: Show Cart Price in Cart Page

--- client/src/components/pages/cart/buy.js
+++ client/src/components/pages/cart/buy.js
@@ -3,18 +3,23 @@ import { services, packages } from '../../../data/data'
 
 const ServiceCard = (props) => {
     const clickHandler = (e) => {
-        props.onHandleClick(e.target.value);
+        props.onHandleClick(e.target.name);
     }
+    const {service} = props;
     return(
+        <div>
+        {service ? (
         <div className='bg-white rounded-md shadow-md p-4 flex flex-wrap items-center justify-start gap-4 mb-2'>
-            <img src={props.service.img} alt={props.service.name} height={"200px"} width={"200px"}/>
-            <div className=' flex-grow'>
-                <h3 className='text-2xl mb-2'>{props.service.name}</h3>
-                <hr />
-                <p className='text-xl mt-2 mb-4'>Base price: {100}</p>
-                <button className='px-4 py-2 bg-blue border-2 border-blue text-white rounded-md hover:bg-white hover:text-blue' onClick={clickHandler} value={props.service.name}>Remove</button>
-            </div>  
+        <img src={service.img} alt={service.name} height={"200px"} width={"200px"}/>
+        <div className=' flex-grow'>
+        <h3 className='text-2xl mb-2'>{service.name}</h3>
+        <hr />
+        <p className='text-xl mt-2 mb-4'>Base price: {100}</p>
+        <button className='px-4 py-2 bg-blue border-2 border-blue text-white rounded-md hover:bg-white hover:text-blue' onClick={clickHandler} value={service.name}>Remove</button>
+        </div>  
         </div>
+        ) : null}
+        </div>   
     )
 }
 
@@ -25,9 +30,12 @@ const Buy = () => {
     Object.keys(items).forEach((name) => {
         const service = services.find(service => service.name === name);
         const item = service ? service : packages.find(p => p.name === name);
+        price += item.price;
         s.push(item);
     })
     const removeItems = (name) => {
+        const item = s.find(item => item.name === name);
+        price -= item.price;
         localStorage.removeItem(name);
         setItems(prevState => {
             return {...localStorage}
@@ -41,7 +49,7 @@ const Buy = () => {
                 <a href='/checkout' className='text-xl bg-blue border-2 border-blue text-white rounded-md px-4 py-2 hover:border-gold hover:text-gold'>Book Appointment</a>
             </div>
             {localStorage.length > 0 ? 
-                s.map((service)=>{return(<ServiceCard service={service} onHandleClick={removeItems}/>)})
+                s.map((service)=>{return(<ServiceCard service={service} onHandleClick={()=> removeItems(service.name)}/>)})
             :<h1 className='text-center my-8 text-2xl'> Your Cart is Empty ! </h1>}
         </div>
     </section>
