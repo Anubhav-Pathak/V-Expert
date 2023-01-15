5d0916d7b56831ee09cc87c2bf5ca64da4ff15d6
Arvind-4 <lordarvindthegamer@gmail.com>
2023-01-15 16:36:29 +0530

    Added: route in app.mjs

--- server/app.mjs
+++ server/app.mjs
@@ -1,13 +1,30 @@
 import express from "express";
 import cors from "cors";
-
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { ApiRouter } from "./routes/api.mjs";
+import * as path from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 app.use('/api', ApiRouter);
+
+app.get("*", function(_, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/build", "index.html"),
+      function (err) {
+        if(err) {
+          res.status(500).send(err)
+        }
+    })
+})
 
 export {
     app
